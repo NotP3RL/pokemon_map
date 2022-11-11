f5e8467d4fb27336e42e12dd604d5f1eaab7723b
NotP3RL <84793664+NotP3RL@users.noreply.github.com>
2022-11-11 18:38:24 +0800

    Добавил описание к покемонам

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 
 class Pokemon(models.Model):
-    title_ru = models.CharField(max_length=200)
-    image = models.ImageField(default='default.png')
+    title_ru = models.CharField('Русское название', max_length=200)
+    image = models.ImageField('Изображение', default='default.png')
+    description = models.TextField('Описание', null=True)
 
     def __str__(self):
-        return self.title
+        return self.title_ru
 
 
 class PokemonEntity(models.Model):
